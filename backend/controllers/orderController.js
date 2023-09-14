15cfaf4527c36ba7611fb4979282f179bcf0022a
murugappansrija <srijamurugappan66@gmail.com>
2023-09-14 16:34:04 +0530

    get-order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -28,3 +28,47 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     order
   })
 });
+
+
+//get single product - api/v1/order/:id
+
+exports.getSingleOrder = catchAsyncError(async(req,res,next)=>{
+  const order =  await Order.findById(req.params.id).populate('user','name email')
+  if(!order){
+    return next (new ErrorHandler(`order not found with this id ${req.body.id}`,404))
+  }
+  res.status(200).json({
+    success:true,
+    order
+  })
+
+})
+
+//get loged in users orders  - api/v1/myorders
+
+exports.myOrders = catchAsyncError(async(req,res,next)=>{
+  const orders = await Order.find({user : req.user.id})
+  res.status(200).json({
+    success:true,
+    orders
+  })
+})
+
+
+//admin: get all orders = api/v1/orders
+
+exports.orders = catchAsyncError(async(req,res,next)=>{
+    const orders =  await Order.find()
+
+    let totalAmount =0
+    orders.forEach(order => {
+      totalAmount += order.totalPrice
+    });
+    res.status(200).json({
+      suc:true,
+      totalAmount,
+      orders
+    })
+})
+
+//admin : update order-> delivered and reduce quantity in products
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const { newOrder } = require('../controllers/orderController')
+const { newOrder,  getSingleOrder, myOrders, orders } = require('../controllers/orderController')
 const router = express.Router()
-const {isAuthenticatedUser} = require('../middlewares/authenticate')
+const {isAuthenticatedUser, autherizeRoles} = require('../middlewares/authenticate')
 
 router.route('/order/new').post(isAuthenticatedUser,newOrder)
+router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder)
+router.route('/myorders').get(isAuthenticatedUser,myOrders)
+router.route('/orders').get(isAuthenticatedUser,autherizeRoles('admin'),orders)
 module.exports =  router
